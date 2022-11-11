a8b8d7ae3d80a0074c473ee520816daf2e8255e9
mahima <076bct033.mahima@pcampus.edu.np>
2022-11-12 02:55:58 +0545

    gardient added

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -10,9 +10,8 @@ const Home = () => {
   const canvasRef = useRef(null);
   return (
     <div>
-      <Header />
-
-      <section id="slider">
+      <section id="slider" className="gradient_back">
+        <Header />
         <div class="container">
           <div class="row">
             <div class="col-sm-12">
@@ -21,80 +20,22 @@ const Home = () => {
                 class="carousel slide"
                 data-ride="carousel"
               >
-                <ol class="carousel-indicators">
-                  <li
-                    data-target="#slider-carousel"
-                    data-slide-to="0"
-                    class="active"
-                  ></li>
-                  <li data-target="#slider-carousel" data-slide-to="1"></li>
-                  <li data-target="#slider-carousel" data-slide-to="2"></li>
-                </ol>
+              
 
                 <div class="carousel-inner">
                   <div class="item active">
-                    <div class="col-sm-6">
-                      <h1>
-                        <span>THE</span>-POST
-                      </h1>
-                      <h2>100% Responsive Design</h2>
-                      <p>
-                        Lorem ipsum dolor sit amet, consectetur adipisicing
-                        elit, sed do eiusmod tempor incididunt ut labore et
-                        dolore magna aliqua.{" "}
-                      </p>
-                      <button type="button" class="btn btn-default get">
-                        Get it now
-                      </button>
-                    </div>
                     <div class="col-sm-6">
                       <ModelViewer
                         scale="2"
                         modelPath={"assets/obj/headphone_modelling3.glb"}
                       />
-
-                      {/* <canvas id="sumreed" ></canvas> */}
-                      {/* <img
-                        src="assets/images/home/pricing.png"
-                        class="pricing"
-                        alt=""
-                      /> */}
                     </div>
                   </div>
 
                   <div class="item">
                     <div class="col-sm-6">
                       <h1>
-                        <span>THE</span>-POST
-                      </h1>
-                      <h2>100% Responsive Design</h2>
-                      <p>
-                        Lorem ipsum dolor sit amet, consectetur adipisicing
-                        elit, sed do eiusmod tempor incididunt ut labore et
-                        dolore magna aliqua.{" "}
-                      </p>
-                      <button type="button" class="btn btn-default get">
-                        Get it now
-                      </button>
-                    </div>
-                    {/* <div class="col-sm-6">
-                      <img
-                        src="assets/images/home/girl2.jpg"
-                        class="girl img-responsive"
-                        alt=""
-                      />
-                      <img
-                        src="assets/images/home/pricing.png"
-                        class="pricing"
-                        alt=""
-                      />
-                    </div> */}
-                  </div>
-
-                  <div class="item">
-                    <div class="col-sm-6">
-                      <h1>
-                        <span>THE</span>-POST
+                        <span>FEA</span>TURES
                       </h1>
                       <h2>Free Ecommerce Template</h2>
                       <p>
@@ -110,18 +51,6 @@ const Home = () => {
                       <button type="button" class="btn btn-default get">
                         Get it now
                       </button>
-                    </div>
-                    <div class="col-sm-6">
-                      <img
-                        src="assets/images/home/girl3.jpg"
-                        class="girl img-responsive"
-                        alt=""
-                      />
-                      <img
-                        src="assets/images/home/pricing.png"
-                        class="pricing"
-                        alt=""
-                      />
                     </div>
                   </div>
                 </div>
@@ -140,11 +69,22 @@ const Home = () => {
                 >
                   <i class="fa fa-angle-right"></i>
                 </a>
+                <ol class="carousel-indicators">
+                  <li
+                    data-target="#slider-carousel"
+                    data-slide-to="0"
+                    class="active"
+                  ></li>
+                  <li data-target="#slider-carousel" data-slide-to="1"></li>
+                  <li data-target="#slider-carousel" data-slide-to="2"></li>
+                </ol>
               </div>
             </div>
           </div>
         </div>
+        
       </section>
+
       <Feature />
 
       <Footer />
