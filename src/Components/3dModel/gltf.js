866092ea962df8921fbc5d4727058f58d253f81e
Nadika Poudel <nadikapoudel16@gmail.com>
2022-11-12 00:03:42 +0545

    3d model loaded properly

--- src/Components/3dModel/gltf.js
+++ src/Components/3dModel/gltf.js
@@ -2,13 +2,13 @@ import React, { useRef, useState } from "react";
 import { useLoader, useFrame } from "@react-three/fiber";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 
-const GltfModel = ({ modelPath, scale = 10, position = [0, 0, 0] }) => {
+const GltfModel = ({ modelPath, scale = 40, position = [-10, -10, -10] }) => {
   const ref = useRef();
   const gltf = useLoader(GLTFLoader, modelPath);
   const [hovered, hover] = useState(false);
 
   // Subscribe this component to the render-loop, rotate the mesh every frame
-  useFrame((state, delta) => (ref.current.rotation.y += 0.1));
+  useFrame((state, delta) => (ref.current.rotation.y += 0.09));
   return (
     <>
       <primitive
--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -91,23 +91,32 @@ const Home = () => {
                 </ol>
 
                 <div class="carousel-inner">
-                  <div class="item active">
+                <div class="item active">
                     <div class="col-sm-6">
                       <h1>
-                        <span>THERICH</span>-SHOP
+                        <span>THE</span>-POST
                       </h1>
-                      <h2>Free E-Commerce Template</h2>
+                      <h2>100% Responsive Design</h2>
                       <p>
                         Lorem ipsum dolor sit amet, consectetur adipisicing
                         elit, sed do eiusmod tempor incididunt ut labore et
                         dolore magna aliqua.{" "}
                       </p>
-                      <ModelViewer scale="8" modelPath={"assets/obj/headphone_modelling3.glb"} />
                       <button type="button" class="btn btn-default get">
                         Get it now
                       </button>
                     </div>
+                    <div class="col-sm-6">
+                      <ModelViewer scale="80" modelPath={"assets/obj/headphone_modelling3.glb"} />
+                      <img
+                        src="assets/images/home/pricing.png"
+                        class="pricing"
+                        alt=""
+                      />
+                    </div>
                   </div>
+
+                  
                   <div class="item">
                     <div class="col-sm-6">
                       <h1>
