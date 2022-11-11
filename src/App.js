02bae2318ec4968ffd394887079ef852b0cdf94f
Nadika Poudel <nadikapoudel16@gmail.com>
2022-11-11 15:33:24 +0545

    Home page rendered

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import "./App.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import { Home } from "./Components/Home/Home";
+import Home  from "./Components/Home/Home";
 import { Payment } from "./Components/Payment/Payment";
 import { Contact } from "./Components/Contact/Contact";
+//import { Description } from "./Components/Description/Desc";
 
 
 function App() {
@@ -14,7 +15,7 @@ function App() {
           <Route path="/" element={<Home/>} />
           <Route path="/contact" element={<Contact/>} />
           <Route path="/payment" element={<Payment/>} />
-          <Route path="/desc" element={<Payment/>} />
+          {/* <Route path="/desc" element={<Description/>} /> */}
         </Routes>
       </Router>
     </div>
--- src/Components/Description/Desc.js
+++ src/Components/Description/Desc.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-export const Desc = () => {
+export default function Desc(){
   return (
     <div>Desc</div>
   )
