6b14e4b79ebf84fdf5eb483552b22c0188ae6328
mahima <076bct033.mahima@pcampus.edu.np>
2022-11-11 13:06:17 +0545

    Routes made

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
+import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Home } from "./Components/Home/Home";
+import { Payment } from "./Components/Payment/Payment";
+import { Contact } from "./Components/Contact/Contact";
 
-import './App.css';
 
 function App() {
   return (
     <div className="App">
-    nadika
+
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home/>} />
+          <Route path="/contact" element={<Contact/>} />
+          <Route path="/payment" element={<Payment/>} />
+          <Route path="/desc" element={<Payment/>} />
+        </Routes>
+      </Router>
     </div>
   );
 }
